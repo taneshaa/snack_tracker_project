72e03178f2038f110ff59be4df40f95c8b6da558
Tanesha Brester <taneshabrester@gmail.com>
2022-08-31 16:26:11 -0700

    error testing

--- snack_tracker_project/settings.py
+++ snack_tracker_project/settings.py
@@ -56,10 +56,9 @@ ROOT_URLCONF = 'snack_tracker_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / "templates",
-            ],
-        # 'DIRS': [BASE_DIR / 'templates',],
+        # 'DIRS': 
+            # ],
+        'DIRS': [BASE_DIR / 'templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,8 +80,8 @@ WSGI_APPLICATION = 'snack_tracker_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
